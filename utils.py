f1885fb9b745046069b4efc15ed619c11e83931d
Zih-Han Shen <shenzihhan@gmail.com>
2025-04-22 00:39:39 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,1 @@
-import requests
-from collections import Counter
 
-def fetch_all_emotion_data(api_url):
-    try:
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            return response.json().get("data", [])
-        return []
-    except Exception as e:
-        print(f"Failed to fetch data: {e}")
-        return []
-
-def summarize_emotions(data):
-    emotion_counter = Counter()
-    for entry in data:
-        emotions = entry.get("emotions", {})
-        emotion_counter.update(emotions)
-    return dict(emotion_counter)
